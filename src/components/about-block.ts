0231c2812f5b9ac9a31845e0aeba077d6a0b4c11
Julius Spencer <jspencer@juliusspencer.co.nz>
2023-10-02 08:15:52 +1300

    Inline updates to about
    Image update for team

--- src/components/about-block.ts
+++ src/components/about-block.ts
@@ -67,7 +67,26 @@ export class AboutBlock extends ThemedElement {
       <div class="container">
         <div>
           <h1 class="container-title">${aboutBlock.title}</h1>
-          <p>${aboutBlock.callToAction.featuredSessions.description}</p>
+          <p>Interested in VertexAI, Kotlin, GCP, Flutter, Angular, Big Query, Firebase, Machine learning, VR/AR, Material Design, SEO, DevOps, Browsers, Career development, or any number of other Google technologies?
+          <br/><br/>
+          Then don’t miss the 2023 Auckland DevFest: It’s a fun & welcoming not-for-profit conference, made for and run by developers and led by GDG Auckland. This year we are partnering with a range of local and international speakers to bring you a spectacular showcase of technologies, practices, and career advice that’ll leave you inspired.
+          <br/><br/>
+          The DevFest is powered by a shared belief that when developers come together to exchange ideas, amazing things can happen!  Join a vibrant and diverse community where everyone can network, share knowledge and enjoy the experience.
+          <br/><br/>
+          We also want to support our attendees who are experiencing hardship. If you need to access a reduced price ticket in order to attend please reach out to one of the GDG Auckland organisers on the GDG Slack to obtain a discount code.
+          <br/><br/>
+          <b>With your DevFest ticket you get:</b>
+          <ul>
+          <li>12+ talks across 2 streams covering Mobile, AI, Cloud, Web, Design, Testing, Software practices, and Career development</li>
+          <li>World class speakers from Google, industry and community speakers</li>
+          <li>Full day conference and after party networking</li>
+          <li>Fun swag to show off to your friends and colleagues!</li>
+          <li>Scrumptious lunch, morning and afternoon tea provided</li>
+          <li>Multiple door prize giveaways during the day</li>
+          <li>Expo booths during breaks so attendees can experience demos and talk to industry professionals</li>
+          <li>Networking opportunities throughout the day and at the after party with potential clients or colleagues</li>
+          </ul>
+          </p>
           <p>${aboutBlock.callToAction.howItWas.description}</p>
         </div>
 
