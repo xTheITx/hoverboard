bab3b481da1353701928755f85134840ea35cb6e
Julius Spencer <jspencer@juliusspencer.co.nz>
2023-10-01 16:44:04 +1300

    Update for updated web application instance

--- src/__mocks__/firebase.ts
+++ src/__mocks__/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyCVxGOmosML19O6P8bse5bSDvx6HW_UeyI',
-  authDomain: 'devfest-auckland.firebaseapp.com',
-  databaseURL: 'https://devfest-auckland.firebaseio.com',
-  projectId: 'devfest-auckland',
-  storageBucket: 'devfest-auckland.appspot.com',
-  messagingSenderId: '958563701490',
-  appId: '1:958563701490:web:0324e472c4cb93bd22f66b',
-  measurementId: 'G-MEASUREMENT_ID',
+  apiKey: "AIzaSyCVxGOmosML19O6P8bse5bSDvx6HW_UeyI",
+  authDomain: "devfest-auckland.firebaseapp.com",
+  databaseURL: "https://devfest-auckland-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "devfest-auckland",
+  storageBucket: "buckets/devfest-auckland.appspot.com",
+  messagingSenderId: "958563701490",
+  appId: "1:958563701490:web:1469d52e0ac6de4322f66b"
 });
 export const db = getFirestore(firebaseApp);
